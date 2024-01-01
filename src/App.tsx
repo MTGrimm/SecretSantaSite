43ffdc3962943a4ef52ea1e65247c622fa7bd5d7
Aryan Timilsina <aryan.timilsina195@gmail.com>
2024-01-01 01:16:00 -0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import './App.css'
+import React, {useRef} from 'react'
+function App() {
+    const fileInputRef = useRef(null);
+
+    const handleSubmit = async() => {
+        const selectedFile = fileInputRef.current.files[0];
+        if (!selectedFile) {
+            console.log("ya doofus it doesn't do it like that");
+            return;
+        }
+
+        const formData = new FormData();
+        formData.append('file', selectedFile);
+
+        try {
+            const response = await fetch("https://secretsantahttp.fly.dev/upload", {
+                method: "POST",
+                body: formData,
+            });
+            
+            if (response.ok) {
+                console.log("things work");
+            } else {
+                console.error("failed");
+            }
+        } catch (error) {
+            console.error("error occurred: ", error);
+        }
+    }
+    
+  return (
+    <div className="mainDiv">
+        <div className="topHalf">
+        </div>
+        <div className="bottomHalf">
+            <input type="file" name="json file" ref={fileInputRef} accept=".json, application/json"/>
+            <button onClick={handleSubmit}>submit:3</button>
+        </div>
+    </div>
+  )
+}
+
+export default App
